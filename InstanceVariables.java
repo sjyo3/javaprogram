6bc4534c03d89be05cb9999b53adc31ce7d682dd
sjyo3 <35940114+sjyo3@users.noreply.github.com>
2018-01-30 21:07:20 -0500

    Add files via upload

---
+++ InstanceVariables.java
@@ -0,0 +1,16 @@
+package com.mypackage;
+
+public class InstanceVariables {
+	int i=3;
+	String s="Sai";
+	int salary=3000;
+	
+	public static void main(String[] args) {
+		InstanceVariables i=new InstanceVariables();
+		
+		System.out.println(i.i);
+		System.out.println(i.s);
+		System.out.println(i.salary);
+	}
+
+}
---
+++ LocalVariables.java
@@ -0,0 +1,18 @@
+package com.mypackage;
+
+public class LocalVariables {
+
+	public static void main(String[] args) {
+		int[] a=new int[5];
+		a[0]=3;
+		a[1]=5;
+		a[2]=7;
+		a[3]=9;
+		a[4]=10;
+		
+		for (int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+}
